10a0f2db4b592ee9e4d79f4521f7cf7dc4f5f185
Bernabe Gonzalez <bernabe@rootstrap.com>
2019-07-05 15:26:52 -0300

    add sync storage example

--- examples/async-storage/src/index.js
+++ examples/async-storage/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Session from 'react-session-persist';
+// import async storage
+import localforage from 'localforage'
 
 import App from './App';
 
 ReactDOM.render(
-  <Session>
+  <Session storage={localforage}>
     <App />
   </Session>,
   document.getElementById('root')
